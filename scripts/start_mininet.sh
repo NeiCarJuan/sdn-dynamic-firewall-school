79313141f324900a9622679044349be82bdded1b
NeiCarJuan <ntrandang1@gmail.com>
2026-01-03 19:52:43 +0700

    Update start_mininet.sh to include Mininet commands

--- scripts/start_mininet.sh
+++ scripts/start_mininet.sh
@@ -3,3 +3,9 @@ set -e
 echo "[+] Starting Mininet topology"
 cd controller/topology
 sudo python3 school_topology.py
+#!/bin/bash
+sudo mn -c
+sudo mn --custom controller/topology/school_topology.py \
+        --topo school \
+        --controller=remote,ip=127.0.0.1,port=6633 \
+        --switch ovs,protocols=OpenFlow13
