07c8f772e0adfe48ebe4a62a4e443b9299455f9e
NeiCarJuan <ntrandang1@gmail.com>
2026-01-03 19:53:07 +0700

    Implement SDN Firewall Controller with flow handling

--- controller/sdn_firewall_app.py
+++ controller/sdn_firewall_app.py
@@ -5,3 +5,89 @@ Ninh: Ryu SDN Firewall main app (OpenFlow)
 - Call modules: collector, zscore, rule_engine, mitigation, learner, logger
 """
 # TODO: implement RyuApp skeleton
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER, set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet
+import logging
+
+LOG_FILE = "controller/logs/controller.log"
+logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
+
+class SDNFirewall(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SDNFirewall, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+        logging.info("SDN Firewall Controller started")
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                           ofproto.OFPCML_NO_BUFFER)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=0,
+            match=match,
+            instructions=inst
+        )
+        datapath.send_msg(mod)
+        logging.info("Default flow installed")
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        src = eth.src
+        dst = eth.dst
+
+        in_port = msg.match['in_port']
+
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            mod = parser.OFPFlowMod(
+                datapath=datapath,
+                priority=1,
+                match=match,
+                instructions=[parser.OFPInstructionActions(
+                    ofproto.OFPIT_APPLY_ACTIONS, actions)]
+            )
+            datapath.send_msg(mod)
+
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=msg.data
+        )
+        datapath.send_msg(out)
+
+        logging.info(f"PacketIn: {src} -> {dst} via switch {dpid}")
