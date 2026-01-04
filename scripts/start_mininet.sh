274e5533d3b78b148fc5187eac52304e653f795e
NeiCarJuan <ntrandang1@gmail.com>
2026-01-04 15:15:00 +0700

    Update start_mininet.sh

--- scripts/start_mininet.sh
+++ scripts/start_mininet.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-echo "[+] Cleaning old Mininet state..."
+echo "[+] Cleaning old Mininet state"
 sudo mn -c
 
-echo "[+] Starting Mininet topology (port=6653)..."
-sudo mn --custom controller/topology/school_topology.py \
-        --topo school \
-        --controller=remote,ip=127.0.0.1,port=6653 \
-        --switch ovs,protocols=OpenFlow13
+echo "[+] Starting School topology (controller 6653)"
+sudo mn \
+  --custom controller/topology/school_topology.py \
+  --topo school \
+  --controller=remote,ip=127.0.0.1,port=6653 \
+  --switch ovs,protocols=OpenFlow13
