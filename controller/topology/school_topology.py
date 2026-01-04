759b7a3aec9616923307d85fd09bebe7f3ad6b25
NeiCarJuan <ntrandang1@gmail.com>
2026-01-04 14:15:29 +0700

    Update school_topology.py

--- controller/topology/school_topology.py
+++ controller/topology/school_topology.py
@@ -4,14 +4,17 @@ class SchoolTopo(Topo):
     def build(self):
         s1 = self.addSwitch('s1')
 
-        h_student = self.addHost('h1', ip='10.0.0.1/24')
-        h_teacher = self.addHost('h2', ip='10.0.0.2/24')
-        h_server  = self.addHost('h3', ip='10.0.0.100/24')
+        # Student
+        h1 = self.addHost('h1', ip='10.0.0.1/24')
 
-        self.addLink(h_student, s1)
-        self.addLink(h_teacher, s1)
-        self.addLink(h_server,  s1)
+        # Accounting department
+        h2 = self.addHost('h2', ip='10.0.0.2/24')
 
-topos = {
-    'school': (lambda: SchoolTopo())
-}
+        # Attacker (internal threat)
+        h3 = self.addHost('h3', ip='10.0.0.100/24')
+
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s1)
+
+topos = { 'school': SchoolTopo }
