6d8c7b6ca481187a4497a3188803eb4ba7d8f302
NeiCarJuan <ntrandang1@gmail.com>
2026-01-04 15:14:40 +0700

    Update start_controller.sh

--- scripts/start_controller.sh
+++ scripts/start_controller.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+echo "[+] Activating Ryu virtualenv"
+source ~/ryu-env/bin/activate
+
 echo "[+] Starting Ryu Controller on port 6653"
-ryu-manager controller/sdn_firewall_app.py --ofp-tcp-listen-port 6653
+cd controller
+ryu-manager sdn_firewall_app.py --ofp-tcp-listen-port 6653
