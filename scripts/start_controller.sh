0a9b24a346d61c6563150e03cf5b1f95b57bcc5b
NeiCarJuan <ntrandang1@gmail.com>
2026-01-04 15:09:48 +0700

    Update start_controller.sh

--- scripts/start_controller.sh
+++ scripts/start_controller.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-echo "[+] Starting Ryu Controller..."
-ryu-manager controller/sdn_firewall_app.py
+echo "[+] Starting Ryu Controller on port 6653"
+ryu-manager controller/sdn_firewall_app.py --ofp-tcp-listen-port 6653
