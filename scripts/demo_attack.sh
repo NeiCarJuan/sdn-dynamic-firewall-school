63066f030b6ca2cf32fb7c27bd0cc8ddf1fc67da
NeiCarJuan <ntrandang1@gmail.com>
2026-01-19 20:53:13 +0700

    Delete scripts/demo_attack.sh

--- scripts/demo_attack.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-echo "[+] Demo attack placeholder"
-echo "Example SYN flood:"
-echo "  hping3 -S -p 80 --flood <target-ip>"
-echo ""
-echo "Example port scan:"
-echo "  nmap -sS <target-ip>"
